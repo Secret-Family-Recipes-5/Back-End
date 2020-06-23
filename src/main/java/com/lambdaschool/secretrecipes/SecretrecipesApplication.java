8d570eae552f954f8bd0a766e4508031d257d929
james-coulter <95coulter@gmail.com>
2020-06-23 00:59:24 -0600

    First Deployment

--- src/main/java/com/lambdaschool/secretrecipes/SecretrecipesApplication.java
+++ src/main/java/com/lambdaschool/secretrecipes/SecretrecipesApplication.java
@@ -1,15 +1,59 @@
 package com.lambdaschool.secretrecipes;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class SecretrecipesApplication {
 
+    /**
+     * Connect to the system environment where environment variables live.
+     */
+    @Autowired
+    private static Environment env;
+
+    /**
+     * If an environment variable is not found, set this to true
+     */
+    private static boolean stop = false;
+
+    /**
+     * If an application relies on an environment variable, check to make sure that environment variable is available!
+     * If the environment variable is not available, you could set a default value, or as is done here, stop execution of the program
+     *
+     * @param envvar The system environment where environment variable live
+     */
+    private static void checkEnvironmentVariable(String envvar)
+    {
+        if (System.getenv(envvar) == null)
+        {
+            stop = true;
+        }
+    }
+
+    /**
+     * Main method to start the application.
+     *
+     * @param args Not used in this application.
+     */
+
+
     public static void main(String[] args) {
-        SpringApplication.run(SecretrecipesApplication.class, args);
+
+        checkEnvironmentVariable("OAUTHCLIENTID");
+        checkEnvironmentVariable("OAUTHCLIENTSECRET");
+
+        if (!stop)
+        {
+            // so run the application!
+            SpringApplication.run(SecretrecipesApplication.class,
+                    args);
+        }
+//        SpringApplication.run(SecretrecipesApplication.class, args);
     }
 
 }
